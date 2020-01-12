21e8e053813e8ac8bd0a14dc39b5c8c1442e36e9
Jack Le Hamster <jacklehamster@gmail.com>
2020-01-12 09:55:47 +0900

    Provide basic offline experience.
    Finish https://codelabs.developers.google.com/codelabs/your-first-pwapp when there's a chance.

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const FILES_TO_CACHE = [
+  'offline.html',
+  'manifest.json',
+];
+
+const CACHE_NAME = "test123456";
+
+addEventListener('install', event => {
+	event.waitUntil(
+	    caches.open(CACHE_NAME).then((cache) => {
+	      return cache.addAll(FILES_TO_CACHE);
+	    })
+	);
+});
+
+addEventListener('activate', event => {
+	event.waitUntil(
+	    caches.keys().then((keyList) => {
+	      return Promise.all(keyList.map((key) => {
+	        if (key !== CACHE_NAME) {
+	          return caches.delete(key);
+	        }
+	      }));
+	    })
+	);
+});
+
+addEventListener('fetch', event => {
+	if (event.request.mode !== 'navigate') {
+	  // Not a page navigation, bail.
+	  return;
+	}
+	event.respondWith(
+	    fetch(event.request)
+	        .catch(() => {
+	          return caches.open(CACHE_NAME)
+	              .then((cache) => {
+	                return cache.match('offline.html');
+	              });
+	        })
+	);
+});
+
